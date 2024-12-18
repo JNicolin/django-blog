37e90b2f6bcc2657c504e4e7c1808255a2d8eb53
Johan Nicolin <johan.nicolin@gmail.com>
2024-12-18 14:55:53 +0000

    Adding allowed host in Settings

--- codestar/urls.py
+++ codestar/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 from blog import views as blog_views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('blog/', blog_views.blog, name="blog"),
+    path('admin/', admin.site.urls),
+    
 ]
